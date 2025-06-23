1a65eb94925dc2e9ca134613bac736e2de6ab57a
Betul Soysal <52222047+betuls@users.noreply.github.com>
2025-06-23 13:59:33 -0700

    Revert "enrollz: add nonce verification logic and tests"

--- service/biz/enrollz_biz_test.go
+++ service/biz/enrollz_biz_test.go
@@ -42,7 +42,6 @@ type stubEnrollzInfraDeps struct {
 	rotateOIakCertReq          *epb.RotateOIakCertRequest
 	verifyIakAndIDevIDCertsReq *VerifyIakAndIDevIDCertsReq
 	verifyTpmCertReq           *VerifyTpmCertReq
-	verifyNonceSignatureReq    *VerifyNonceSignatureReq
 
 	// Stubbed responses to simulate behavior of deps without implementing them.
 	issueOwnerIakCertResp       *IssueOwnerIakCertResp
@@ -51,7 +50,6 @@ type stubEnrollzInfraDeps struct {
 	rotateOIakCertResp          *epb.RotateOIakCertResponse
 	verifyIakAndIDevIDCertsResp *VerifyIakAndIDevIDCertsResp
 	verifyTpmCertResp           *VerifyTpmCertResp
-	verifyNonceSignatureResp    *VerifyNonceSignatureResp
 
 	// If we need to simulate an error response from any of the deps, then set
 	// the dep's response to nil and populate this error field.
@@ -140,23 +138,6 @@ func (s *stubEnrollzInfraDeps) RotateOIakCert(ctx context.Context, req *epb.Rota
 		return nil, s.errorResp
 	}
 	return s.rotateOIakCertResp, nil
-}
-
-func (s *stubEnrollzInfraDeps) VerifyNonceSignature(ctx context.Context, req *VerifyNonceSignatureReq) (*VerifyNonceSignatureResp, error) {
-	// Validate that no stub (captured) request params were set prior to execution.
-	if s.verifyNonceSignatureReq != nil {
-		return nil, fmt.Errorf("VerifyNonceSignature unexpected req %+v", req)
-	}
-	s.verifyNonceSignatureReq = req
-
-	// If a stubbed response is not set, then return error, otherwise return the response.
-	if s.verifyNonceSignatureResp == nil {
-		return nil, s.errorResp
-	}
-	if !s.verifyNonceSignatureResp.IsValid {
-		return s.verifyNonceSignatureResp, s.errorResp
-	}
-	return s.verifyNonceSignatureResp, nil
 }
 
 func TestEnrollControlCard(t *testing.T) {
@@ -614,208 +595,6 @@ func TestRotateOwnerIakCert(t *testing.T) {
 			}
 			if diff := cmp.Diff(stub.rotateOIakCertReq, test.wantRotateOIakCertReq, protocmp.Transform()); diff != "" {
 				t.Errorf("RotateOIakCertRequest request param to stubbed RotateOIakCert dep does not match expectations: diff = %v", diff)
-			}
-		})
-	}
-}
-
-func boolPtr(b bool) *bool {
-	return &b
-}
-
-func TestNonceVerification(t *testing.T) {
-	// Constants to be used in request params and stubbing.
-	controlCardSelection := &cpb.ControlCardSelection{
-		ControlCardId: &cpb.ControlCardSelection_Role{
-			Role: cpb.ControlCardRole_CONTROL_CARD_ROLE_ACTIVE,
-		},
-	}
-	certVerificationOpts := x509.VerifyOptions{
-		DNSName: "Some DNS name",
-	}
-	vendorID := &cpb.ControlCardVendorId{
-		ControlCardRole:     controlCardSelection.GetRole(),
-		ControlCardSerial:   "Some card serial",
-		ControlCardSlot:     "Some card slot",
-		ChassisManufacturer: "Some manufacturer",
-		ChassisPartNumber:   "Some part",
-		ChassisSerialNumber: "Some chassis serial",
-	}
-	sslProfileID := "some-ssl-profile-id"
-	iakCert := "Some IAK cert PEM"
-	iDevIDCert := "Some IDevID cert PEM"
-	iakPub := "Some IAK pub PEM"
-	iDevIDPub := "Some IDevID pub PEM"
-	oIakCert := "Some Owner IAK cert PEM"
-	oIdevIDCert := "Some Owner IDevID cert PEM"
-	errorResp := errors.New("Some error")
-	validNonceSignature := []byte("some-nonce-signature")
-
-	tests := []struct {
-		// Test description.
-		desc string
-		// Indicates if the test is for EnrollControlCard or RotateOwnerIakCert
-		isEnrollmentTest bool
-		// Overall expected EnrollControlCard response.
-		wantErrResp error
-		// Stubbed responses to EnrollzInfraDeps deps.
-		getIakCertResp           *epb.GetIakCertResponse
-		verifyNonceSignatureResp *VerifyNonceSignatureResp
-		skipNonceExchange        *bool
-		// Expected verifyNonceSignatureReq to be called.
-		wantVerifyNonceSignatureReq bool
-	}{
-		{
-			desc:             "Successful nonce verification",
-			isEnrollmentTest: true,
-			getIakCertResp: &epb.GetIakCertResponse{
-				ControlCardId:  vendorID,
-				IakCert:        iakCert,
-				IdevidCert:     iDevIDCert,
-				NonceSignature: validNonceSignature,
-			},
-			verifyNonceSignatureResp: &VerifyNonceSignatureResp{
-				IsValid: true,
-			},
-			wantVerifyNonceSignatureReq: true,
-			skipNonceExchange:           boolPtr(false),
-			wantErrResp:                 nil,
-		},
-		{
-			desc:             "Failed nonce verification",
-			isEnrollmentTest: true,
-			wantErrResp:      errorResp,
-			getIakCertResp: &epb.GetIakCertResponse{
-				ControlCardId:  vendorID,
-				IakCert:        iakCert,
-				IdevidCert:     iDevIDCert,
-				NonceSignature: validNonceSignature,
-			},
-			verifyNonceSignatureResp: &VerifyNonceSignatureResp{
-				IsValid: false,
-			},
-			wantVerifyNonceSignatureReq: true,
-			skipNonceExchange:           boolPtr(false),
-		},
-		{
-			desc:             "Missing nonce signature, verification skipped",
-			isEnrollmentTest: true,
-			getIakCertResp: &epb.GetIakCertResponse{
-				ControlCardId: vendorID,
-				IakCert:       iakCert,
-				IdevidCert:    iDevIDCert,
-			},
-			skipNonceExchange: boolPtr(false),
-		},
-		{
-			desc:             "Skip Nonce Exchange",
-			isEnrollmentTest: true,
-			getIakCertResp: &epb.GetIakCertResponse{
-				ControlCardId: vendorID,
-				IakCert:       iakCert,
-				IdevidCert:    iDevIDCert,
-			},
-			skipNonceExchange: boolPtr(true),
-		},
-		{
-			desc:             "Successful nonce verification for RotateOwnerIakCert",
-			isEnrollmentTest: false,
-			getIakCertResp: &epb.GetIakCertResponse{
-				ControlCardId:  vendorID,
-				IakCert:        iakCert,
-				NonceSignature: validNonceSignature,
-			},
-			verifyNonceSignatureResp: &VerifyNonceSignatureResp{
-				IsValid: true,
-			},
-			wantVerifyNonceSignatureReq: true,
-			skipNonceExchange:           boolPtr(false),
-			wantErrResp:                 nil,
-		},
-		{
-			desc:             "Failed nonce verification for RotateOwnerIakCert",
-			isEnrollmentTest: false,
-			wantErrResp:      errorResp,
-			getIakCertResp: &epb.GetIakCertResponse{
-				ControlCardId:  vendorID,
-				IakCert:        iakCert,
-				NonceSignature: validNonceSignature,
-			},
-			verifyNonceSignatureResp: &VerifyNonceSignatureResp{
-				IsValid: false,
-			},
-			wantVerifyNonceSignatureReq: true,
-			skipNonceExchange:           boolPtr(false),
-		},
-		{
-			desc:             "Missing nonce signature for RotateOwnerIakCert, verification skipped",
-			isEnrollmentTest: false,
-			getIakCertResp: &epb.GetIakCertResponse{
-				ControlCardId: vendorID,
-				IakCert:       iakCert,
-			},
-			skipNonceExchange: boolPtr(false),
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.desc, func(t *testing.T) {
-			stub := &stubEnrollzInfraDeps{
-				getIakCertResp:              test.getIakCertResp,
-				verifyIakAndIDevIDCertsResp: &VerifyIakAndIDevIDCertsResp{IakPubPem: iakPub, IDevIDPubPem: iDevIDPub},
-				issueOwnerIakCertResp:       &IssueOwnerIakCertResp{OwnerIakCertPem: oIakCert},
-				issueOwnerIDevIDCertResp:    &IssueOwnerIDevIDCertResp{OwnerIDevIDCertPem: oIdevIDCert},
-				rotateOIakCertResp:          &epb.RotateOIakCertResponse{},
-				errorResp:                   test.wantErrResp,
-				verifyNonceSignatureResp:    test.verifyNonceSignatureResp,
-				verifyTpmCertResp:           &VerifyTpmCertResp{PubPem: iakPub},
-			}
-			ctx := context.Background()
-			var got error
-			if test.isEnrollmentTest {
-				req := &EnrollControlCardReq{
-					ControlCardSelection: controlCardSelection,
-					CertVerificationOpts: certVerificationOpts,
-					Deps:                 stub,
-					SSLProfileID:         sslProfileID,
-					SkipNonceExchange:    test.skipNonceExchange,
-				}
-				got = EnrollControlCard(ctx, req)
-			} else {
-				req := &RotateOwnerIakCertReq{
-					ControlCardSelection: controlCardSelection,
-					CertVerificationOpts: certVerificationOpts,
-					Deps:                 stub,
-					SkipNonceExchange:    test.skipNonceExchange,
-				}
-				got = RotateOwnerIakCert(ctx, req)
-			}
-
-			// Verify that EnrollControlCard returned expected error/no-error response.
-			if test.wantErrResp != nil && test.wantErrResp != errors.Unwrap(got) {
-				t.Errorf("Expected error response %v, but got error response %v", test.wantErrResp, errors.Unwrap(got))
-			} else if test.wantErrResp == nil && got != nil {
-
-				t.Errorf("Expected no-error response, but got error response %v", got)
-			}
-
-			// Verify that GetIakCertReq was called with a nonce if SkipNonceExchange is false.
-			if test.skipNonceExchange == nil || !*test.skipNonceExchange {
-				if stub.getIakCertReq == nil || len(stub.getIakCertReq.Nonce) == 0 {
-					t.Errorf("GetIakCertRequest was called without a nonce, but it was expected")
-				}
-			} else if stub.getIakCertReq != nil && len(stub.getIakCertReq.Nonce) > 0 {
-				t.Errorf("GetIakCertRequest was called with a nonce, but it was not expected")
-			}
-
-			// Verify that VerifyNonceSignature was called if expected.
-			if test.wantVerifyNonceSignatureReq && stub.verifyNonceSignatureReq == nil {
-				t.Errorf("VerifyNonceSignature was expected to be called but was not")
-			} else if !test.wantVerifyNonceSignatureReq && stub.verifyNonceSignatureReq != nil {
-				t.Errorf("VerifyNonceSignature was not expected to be called but was")
-			}
-			if test.wantVerifyNonceSignatureReq && (len(stub.verifyNonceSignatureReq.Nonce) == 0 || len(stub.verifyNonceSignatureReq.Signature) == 0 || len(stub.verifyNonceSignatureReq.IAKPubPem) == 0 || stub.verifyNonceSignatureReq.HashAlgo == 0) {
-				t.Errorf("VerifyNonceSignature was expected to be called with all parameters, but one or more parameters was missing")
 			}
 		})
 	}
